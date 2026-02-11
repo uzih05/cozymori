fc9b83c565c79f95e2a9601c2672d56b25267ce3
UZIH05 <luv.wlgjs@gmail.com>
2026-02-12 00:26:34 +0900

    add i18n setup with Korean and English translations

---
+++ src/i18n/routing.ts
@@ -0,0 +1,9 @@
+import { defineRouting } from 'next-intl/routing';
+
+export const routing = defineRouting({
+  locales: ['en', 'ko'],
+  defaultLocale: 'en',
+  localePrefix: 'as-needed',
+  localeDetection: false,
+  localeCookie: false,
+});
